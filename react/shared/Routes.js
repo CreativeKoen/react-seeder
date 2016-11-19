71017a6759364d9e3133f67c8a0f849d2ba879df
creativekoen <creativekoen@gmail.com>
2016-11-19 19:39:10 +0100

    commented the admin blog container

--- react/shared/Routes.js
+++ react/shared/Routes.js
@@ -5,7 +5,7 @@ import { Route, IndexRoute } from 'react-router';
 import BlogContainer from '../Blog/Blog.container';
 import PostContainer from '../Post/Post.container';
 
-import AdminBlogContainer from '../Admin/Blog/Blog.browse.container';
+//import AdminBlogContainer from '../Admin/Blog/Blog.browse.container';
 
 export default (
   <Route path="/">
