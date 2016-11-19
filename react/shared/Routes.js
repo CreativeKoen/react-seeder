9d8d2de979a48837f0dd03cd7c5985ae5ec140c8
CreativeKoen <CreativeKoen@gmail.com>
2016-11-19 19:47:18 +0100

    add flow-bin and add flow-typed folder to dit gitignore file

--- react/shared/Routes.js
+++ react/shared/Routes.js
@@ -5,12 +5,12 @@ import { Route, IndexRoute } from 'react-router';
 import BlogContainer from '../Blog/Blog.container';
 import PostContainer from '../Post/Post.container';
 
-import AdminBlogContainer from '../Admin/Blog/Blog.browse.container';
+// import AdminBlogContainer from '../Admin/Blog/Blog.browse.container';
 
 export default (
   <Route path="/">
     <IndexRoute component={BlogContainer}></IndexRoute>
     <Route path="/:postID" component={PostContainer}></Route>
-    // <Route path="/admin/blog" component={AdminBlogContainer}></Route>
+    {/* // <Route path="/admin/blog" component={AdminBlogContainer}></Route> */}
   </Route>
 )
