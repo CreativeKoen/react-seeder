b2a3a3005bcebc1ede3842d329c84426b0b2b717
CreativeKoen <CreativeKoen@gmail.com>
2016-11-21 11:42:21 +0100

    fix indenting

--- react/Post/Post.item.js
+++ react/Post/Post.item.js
@@ -1,18 +1,17 @@
-'use strict';
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 const PostItem = ({ post }) => {
   return (
-      <div className="col-xs-12 col-sm-6 col-md-4">
-        <div className="card card-block">
-          <h2 className="card-title" key={post.id}>{post.title}</h2>
-          <p className="card-text">{post.body}</p>
-          <Link to={`/${post.id}`} className="btn btn-primary">
-            Read More
-          </Link>
-        </div>
+    <div className="col-xs-12 col-sm-6 col-md-4">
+      <div className="card card-block">
+        <h2 className="card-title" key={post.id}>{post.title}</h2>
+        <p className="card-text">{post.body}</p>
+        <Link to={`/${post.id}`} className="btn btn-link">
+          Read More
+        </Link>
       </div>
+    </div>
   )
 }
 
